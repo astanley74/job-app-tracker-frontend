d8febacd9648c3dcaca22da5d6f2ae114c854917
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-04-25 14:14:42 -0400

    add render to users container

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export default class UsersContainer extends React.Component {
-    
+
+    render() {
+        return(
+            <div>
+                UsersContainer
+            </div>
+        )
+    }
+
 }
