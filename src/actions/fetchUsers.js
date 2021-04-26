ea97421bb4ec1bc0b6b0ffa5064eb38ba012d77c
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-04-25 22:02:40 -0400

    create addUser action

--- src/actions/fetchUsers.js
+++ src/actions/fetchUsers.js
@@ -1,3 +1,14 @@
-export function fetchUsers() {
-
+export const addUser = (data) => {
+    return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/users', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        .then(resp => resp.json())
+        .then(user => dispatch({type: "ADD_USER", payload: user}))
+    }
 }
