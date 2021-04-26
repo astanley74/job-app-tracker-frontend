a275388c4ed2cd1ea7705a688fd02e21afdc5b8b
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-04-26 16:56:05 -0400

    add functionality to job applications container

--- src/containers/JobApplicationsContainer.js
+++ src/containers/JobApplicationsContainer.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import JobAppCard from '../components/JobAppCard'
 
 
 class JobApplicationsContainer extends React.Component {
+
+    generateJobAppCards = () => {
+        const { user } = this.props
+        return user.job_applications.map((job_application, index) => <JobAppCard
+            key={index}
+            comapny={job_application.company}
+            date={job_application.date_of_application}
+            position={job_application.position}
+            application_status={job_application.application_status}
+            current_stage={job_application.current_stage}
+            notes={job_application.notes}
+            />)
+    }
+
     render() {
         return(
-            <div>{console.log(this.props.user.job_applications)}</div>
+            <div></div>
         )
     }
 }
