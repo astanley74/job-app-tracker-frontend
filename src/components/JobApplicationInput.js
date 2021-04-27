520c2794d5568075808693ae15e6ccae709d50d7
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-04-26 22:51:02 -0400

    set initial state of JobApplicationInput

--- src/components/JobApplicationInput.js
+++ src/components/JobApplicationInput.js
@@ -4,6 +4,20 @@ import {connect} from 'react-redux'
 import {addJobApplication} from '../actions/addJobApplication'
 
 class JobApplicationInput extends React.Component {
+
+    constructor(){
+        super()
+        this.state = {
+            company_name: "",
+            date_of_application: "",
+            position: "",
+            application_status: true,
+            current_state: "",
+            notes: "",
+            user_id: null
+        }
+    }
+
     render() {
         return(
             <div></div>
