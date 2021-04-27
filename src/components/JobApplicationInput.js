e1887e4b2645f3f86d78b3846a9a11ee67ef8689
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-04-26 22:45:25 -0400

    setup JobApplicationInput

---
+++ src/components/JobApplicationInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class JobApplicationInput extends React.Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+
+
+export default JobApplicationInput
