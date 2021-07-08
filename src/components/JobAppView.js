9729c18786192742cde805e6030f1235d21ef03b
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-07-08 12:03:44 -0400

    add JobAppView component

---
+++ src/components/JobAppView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class JobAppView extends React.Component {
+    
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default JobAppView
