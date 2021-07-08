41584892a7fac12623337f4fbb47d407064278ea
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-07-08 12:16:15 -0400

    connect the JobAppView component to redux store

--- src/components/JobAppView.js
+++ src/components/JobAppView.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 class JobAppView extends React.Component {
-    
+
+
     render() {
+        
         return(
             <div></div>
         )
     }
 }
 
-export default JobAppView
+const mapStateToProps = (state) => {
+    return {job_apps: state.user.job_applications}
+}
+export default connect(mapStateToProps)(JobAppView)
