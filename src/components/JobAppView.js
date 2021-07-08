b46cbdf419cb3eb9a212c3b53de656c4d962a507
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-07-08 15:06:35 -0400

    add styling to JobAppView component

--- src/components/JobAppView.js
+++ src/components/JobAppView.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { Card, Button } from 'react-bootstrap';
+import NavBar from './Navbar';
 
 const JobAppView = (props) => {
     let job_app = props.job_apps.filter(job => job.id === parseInt(props.match.params.jobId))[0]
     console.log(job_app)
         return(
-            <div>{job_app.company_name}</div>
+            <div>
+            <NavBar/>
+            <Card>
+            <Card.Header>{job_app.company_name}</Card.Header>
+            <Card.Body>
+                <Card.Title>{job_app.position}</Card.Title>
+                <Card.Text>
+                    Notes: {job_app.notes}
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+            </Card>
+            </div>
         )
 }
 
